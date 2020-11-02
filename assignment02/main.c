7c1b5f6988b7d9b7ac8ca717a734d91f5e3a47b3
seattlepug <35910559+seattlepug@users.noreply.github.com>
2020-11-01 17:17:01 -0800

    Create main.c

---
+++ assignment02/main.c
@@ -0,0 +1,67 @@
+ // below is section of the code that 
+ // answers Q5
+// int counter = 0;
+
+ // below is section of the code that 
+ // answers Q6 and Q7
+int counter = 0x0;
+
+int main()
+{
+/*
+    This is the trial run 1, for assignment 02
+    Author: Zhe Li
+    Email: lizhe.uw@gmail.com
+*/
+    
+ // below is section of the code that 
+ // answers Q1 and Q2
+ /*
+    int counter = 0;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    return 0;
+*/
+    
+ // below is section of the code that 
+ // answers Q3 and Q4
+ /*
+    unsigned int counter = 0;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    return 0;
+ */
+ 
+ // below is section of the code that 
+ // answers Q5
+ /*
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    counter++;
+    return 0; 
+*/
+
+ // below is section of the code that 
+ // answers Q6 and Q7
+    int *p_int = (int*)0x20000000;
+    ++(*p_int);
+    ++(*p_int);
+    ++(*p_int);
+    counter++;
+    return 0;
+    
+}
