bac8dd5e59dcb15e39da5fb2333338de4a78c200
seattlepug <35910559+seattlepug@users.noreply.github.com>
2020-12-06 19:26:38 -0800

    Create main.c

---
+++ assignment06/Assembly_Code/main.c
@@ -0,0 +1,69 @@
+// this is the answer for assignment06
+// it is based on starting code provided
+// note that all three files are updated accordingly for this assignment
+// main.c, delay.s, and user_led.s files
+// all three are submitted
+// but there are no real changes to the main.c file
+// I only changed the duration to make sure my code works at different versions
+
+#include <stdint.h>
+
+// LD1 --> PA5 -- Homework
+// LD2 --> PB14 -- In Class
+
+#define RCC_BASE 0x40021000
+#define RCC_AHB2ENR (*((unsigned int*)(RCC_BASE + 0x4C)))
+
+#define GPIOB_BASE 0x48000400
+#define GPIOB_MODER (*((unsigned int*)(GPIOB_BASE + 0x00)))
+#define GPIOB_ODR (*((unsigned int*)(GPIOB_BASE + 0x14)))
+
+#define GPIOA_BASE 0x48000000
+#define GPIOA_MODER (*((unsigned int*)(GPIOA_BASE + 0x00)))
+#define GPIOA_ODR (*((unsigned int*)(GPIOA_BASE + 0x14)))
+
+#define ORD14   (1<<14)
+#define ORD5    (1<<5)
+
+#define LED_ON 1
+#define LED_OFF 0
+#define DELAY_DURATION 500000
+
+void delay(uint32_t iteration);
+void control_user_led1(uint8_t state, uint32_t duration);
+// void enable_rcc(uint32_t port);
+
+void main(void)
+{    
+    // RCC AHB2 peripheral clock enable register (RCC_AHB2ENR)
+    // RCC Base Address: 0x40021000
+    // Address offset: 0x4C
+    // Set bit[1] to 1 to enable clock to PortB
+    // Set bit[0] to 1 to enable clock to PortA
+    // 1. Enable clock to Peripheral
+    
+    RCC_AHB2ENR |= 0x1;
+    
+    // <TODO> Replace above statement with function call below
+    // enable_rcc(0);
+  
+    // GPIO port mode register (GPIOx_MODER) (x = A..E and H)
+    // GPIOA Base Address: 0x48000000
+    // Address offset: 0x00
+    // Set bit[11:10] to 0x01  so --> 0x400 // To enable PA5 as output
+    // Note: GPIOA_MODER reset value = 0xABFFFFFF
+    //       So will only need to clear bit 11
+    GPIOA_MODER &= 0xFFFFF7FF;
+
+    // GPIO port output data register (GPIOx_ODR) (x = A..E and H)
+    // GPIOA Base Address: 0x48000000
+    // Address offset: 0x14
+    // Set bit[5] to 1 --> 0x20; // Turn LED ON
+    // Set bit[5] to 0 --> 0x0; // Turn LED OFF
+     
+    while(1)
+    {
+        control_user_led1(LED_ON, DELAY_DURATION);
+        control_user_led1(LED_OFF, DELAY_DURATION);
+    }
+}
